9b35ea769612325bbc93cb51936f97aa56ca83a2
manlikestiffler <tinashegomo96@gmail.com>
2024-04-01 16:48:08 +0200

    Update settings.py

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = 'https://projectmanagementsystem1.onrender.com'
+
 
 
 # Application definition
