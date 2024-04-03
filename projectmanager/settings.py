c3bf7ca1531e75e5fecd66c5b99ea3da0e7f67cf
manlikestiffler <tinashegomo96@gmail.com>
2024-04-03 16:08:31 +0200

    a

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', 'projectmanagementsystem1.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'projectmanagementsystem1.onrender.com']
 # RENDER_EXTERNAL_HOSTNAME=os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 # if RENDER_EXTERNAL_HOSTNAME:
 #     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
